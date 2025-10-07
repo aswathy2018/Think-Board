6d85c044c860f87bc082f277ebd473861d37401a
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-10-07 18:35:38 +0530

    RateLimit added

--- backend/src/controllers/noteControllers.js
+++ backend/src/controllers/noteControllers.js
@@ -4,12 +4,30 @@ import Note from "../models/Note.js"
 export async function getAllNotes (req, res) {
     try {
 
-        const notes = await Note.find()
+        const notes = await Note.find().sort({createdAt: -1})
         res.status(200).json(notes)
 
     } catch (error) {
 
         console.error("Error in getAllNotes controller", error)
+        res.status(500).json({message: "Internal server error"})
+
+    }
+}
+
+
+export async function getNoteById (req, res) {
+    try {
+        const note = await Note.findById(req.params.id)
+
+        if(!note){
+            return res.status(404).json({message: "Note not found"})
+        }
+
+        res.status(200).json(note)
+    } catch (error) {
+        
+        console.error("Error in getNoteById controller", error)
         res.status(500).json({message: "Internal server error"})
 
     }
@@ -37,6 +55,7 @@ export async function createNote (req, res) {
     }
 }
 
+
 export async function updateNote (req, res) {
     try {
 
@@ -56,6 +75,7 @@ export async function updateNote (req, res) {
 
     }
 }
+
 
 export async function deleteNote (req, res) {
     try {
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,21 @@ import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/ratelimiter.js";
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
-connectDB();
 
 
+//middleware
 app.use(express.json())
-
+app.use(rateLimiter)
 app.use("/api/notes", noteRoutes);
 
-
-app.listen(PORT, ()=>{
-    console.log("Server started on port",PORT)
-});
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Server started on port",PORT)
+    });
+})
