1b6eec88347fabdbd3f498148245de4be45da948
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-10-10 18:18:32 +0530

    RateLimitUi added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,9 +8,10 @@ const NavBar = () => {
       <div className='flex items-center justify-between'>
         <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>ThinkBoard</h1>
         <div className='flex items-center gap-4'>
-          <Link to = {"/create"} className='btn btn-primary'/>
+          <Link to = {"/create"} className='btn btn-primary'>
           <PlusIcon className='size-5'/>
           <span>New Note</span>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/NavBar'
+import RateLimited from '../components/RateLimited'
 
 const HomePage = () => {
+  const {isRateLimited, setIsRateLimited} = useState(true)
   return (
     <div className='min-h-screen'>
       <Navbar/>
+      {isRateLimited && <RateLimited/>}
     </div>
   )
 }
