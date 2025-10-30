afd20e5428b1159081a794ac03e42f4db2014333
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-10-30 20:01:03 +0530

    Home page done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/NavBar'
 import RateLimited from '../components/RateLimited.jsx'
+import NoteCard from '../components/NoteCard.jsx'
 import axios from 'axios'
 import toast from "react-hot-toast"
 
@@ -42,6 +43,16 @@ const HomePage = () => {
     <div className='min-h-screen'>
       <Navbar />
       {isRateLimited && <RateLimited />}
+      <div className='max-w-7xl mx-auto p-4 mt-6'></div>
+      {loading && <div className='text-center text-primary py-40'>Loading Notes..</div>}
+
+      {notes.length>0 && !isRateLimited && (
+        <div className='ml-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {notes.map((note) => (
+            <NoteCard key={note._id} note={note}/>            
+          ))}
+        </div>
+      )}
     </div>
   )
 }
