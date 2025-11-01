c8e9108d7235b54f8d6ce2342d0a0e4f0bb66587
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-11-01 12:01:44 +0530

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ dotenv.config()
 console.log("ENV TEST:", process.env.UPSTASH_REDIS_REST_URL, process.env.UPSTASH_REDIS_REST_TOKEN);
 import express from "express";
 import cors from "cors";
+import path from "path"
 
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,14 +12,17 @@ import rateLimiter from "./middleware/ratelimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve()
 
 //middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-)
+if(process.env.NODE_ENV !== "production"){
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    )
+}
+
 app.use(express.json())
 app.use(rateLimiter)
 
@@ -26,6 +30,18 @@ app.use(rateLimiter)
 
 app.use("/api/notes", noteRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    // })
+
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+
+}
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
